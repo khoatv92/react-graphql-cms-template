5a7fc3180febd69c0f46f6f3138a0a9de78ed026
Van Khoa <khoatv@appota.com>
2023-01-04 14:52:12 +0700

    Integration apexcharts

---
+++ src/components/Chart/Dashboard.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import Chart from 'react-apexcharts';
+import { Col } from 'antd';
+import { ApexOptions } from 'apexcharts';
+
+const randomizeArray = function (arg: Array<number>) {
+  const array = arg.slice();
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
+
+const sparklineData: Array<number> = [
+  47, 45, 54, 38, 56, 24, 65, 31, 37, 39, 62, 51, 35, 41, 35, 27, 93, 53, 61,
+  27, 54, 43, 19, 46
+];
+
+const labels = [...Array(24).keys()].map((n) => `2018-09-0${n + 1}`);
+
+interface Props {
+  subtitle: string;
+  title: string;
+}
+
+const ChartDashboard = ({ subtitle, title }: Props) => {
+  const series = [
+    {
+      name: 'Sales',
+      data: randomizeArray(sparklineData)
+    }
+  ];
+
+  const options: ApexOptions = {
+    chart: {
+      id: 'sparkline',
+      group: 'sparklines',
+      type: 'area',
+      height: 160,
+      sparkline: {
+        enabled: true
+      }
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    fill: {
+      opacity: 1
+    },
+    series: [
+      {
+        name: 'Sales',
+        data: randomizeArray(sparklineData)
+      }
+    ],
+    labels: labels,
+    yaxis: {
+      show: false
+    },
+    xaxis: {
+      type: 'category'
+    },
+    colors: ['#008FFB'],
+    title: {
+      text: title,
+      style: {
+        fontSize: '24px'
+      }
+    },
+    subtitle: {
+      text: subtitle,
+      style: {
+        fontSize: '14px'
+      }
+    }
+  };
+
+  return (
+    <Col xs={24} lg={8}>
+      <Chart options={options} series={series} type="area" height={150} />
+    </Col>
+  );
+};
+
+export default ChartDashboard;
--- src/containers/Index.tsx
+++ src/containers/Index.tsx
@@ -1,35 +1,61 @@
 import React, { useState } from 'react';
 import { ProCard } from '@ant-design/pro-components';
-import { Statistic, Divider } from 'antd';
+import { Statistic, Divider, Row, Card } from 'antd';
 import RcResizeObserver from 'rc-resize-observer';
+import ChartDashboard from 'components/Chart/Dashboard';
+
+const chart = [
+  { title: '$424,652', subtitle: 'Sales' },
+  { title: '$235,312', subtitle: 'Expenses' },
+  { title: '$135,965', subtitle: 'Profits' }
+];
 
 const Index = () => {
   const [responsive, setResponsive] = useState(false);
+
   return (
-    <RcResizeObserver
-      key="resize-observer"
-      onResize={(offset) => {
-        setResponsive(offset.width < 596);
-      }}
-    >
-      <ProCard.Group title="核心指标" direction={responsive ? 'column' : 'row'}>
-        <ProCard>
-          <Statistic title="今日UV" value={79.0} precision={2} />
-        </ProCard>
-        <Divider type={responsive ? 'horizontal' : 'vertical'} />
-        <ProCard>
-          <Statistic title="冻结金额" value={112893.0} precision={2} />
-        </ProCard>
-        <Divider type={responsive ? 'horizontal' : 'vertical'} />
-        <ProCard>
-          <Statistic title="信息完整度" value={93} suffix="/ 100" />
-        </ProCard>
-        <Divider type={responsive ? 'horizontal' : 'vertical'} />
-        <ProCard>
-          <Statistic title="冻结金额" value={112893.0} />
-        </ProCard>
-      </ProCard.Group>
-    </RcResizeObserver>
+    <>
+      <RcResizeObserver
+        key="resize-observer"
+        onResize={(offset) => {
+          setResponsive(offset.width < 596);
+        }}
+      >
+        <ProCard.Group
+          bordered
+          title="Overview"
+          direction={responsive ? 'column' : 'row'}
+        >
+          <ProCard>
+            <Statistic title="今日UV" value={79.0} precision={2} />
+          </ProCard>
+          <Divider type={responsive ? 'horizontal' : 'vertical'} />
+          <ProCard>
+            <Statistic title="冻结金额" value={112893.0} precision={2} />
+          </ProCard>
+          <Divider type={responsive ? 'horizontal' : 'vertical'} />
+          <ProCard>
+            <Statistic title="信息完整度" value={93} suffix="/ 100" />
+          </ProCard>
+          <Divider type={responsive ? 'horizontal' : 'vertical'} />
+          <ProCard>
+            <Statistic title="冻结金额" value={112893.0} />
+          </ProCard>
+        </ProCard.Group>
+      </RcResizeObserver>
+      <br />
+      <Card bordered title="Chart">
+        <Row gutter={[24, 24]}>
+          {chart.map((item) => (
+            <ChartDashboard
+              key={item.title}
+              title={item.title}
+              subtitle={item.subtitle}
+            />
+          ))}
+        </Row>
+      </Card>
+    </>
   );
 };
 
